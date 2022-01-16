5723a6a544a84f602e9784622d628221966128d6
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-01-16 16:23:08 +0000

    new message checker

--- main.py
+++ main.py
@@ -12,26 +12,38 @@ nikl = '@lizk1a1'
 nikr = '@r4419'
 rty=0
 
+last_user = 0
+last_message = ''
+
+def check_new_message(user, message):
+    if (last_user == user and message == last_message):
+        return False
+    else: return True
+    
+
+
 @bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Заскучали?')
+def start(message, res=False):
+    if(check_new_message(message.chat.id, message.text)):
+        bot.send_message(message.chat.id, 'Заскучали?')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text[0:2] == 'оп':
-        if message.text[2] == 'л':
-            bot.send_message(message.chat.id, nikl)
-        elif message.text[2] == 'д':
-            bot.send_message(message.chat.id, nikd)
-        elif message.text[2] == 'ж':
-            bot.send_message(message.chat.id, nikg)
-        elif message.text[2] == 'р':
-            bot.send_message(message.chat.id, nikr)
-    elif (message.text).lower() == 'квожка':
-        bot.send_message(message.chat.id, 'Квожка здесь, чё пристали?')
-    elif message.text == "*+":
-        bot.send_message(message.chat.id, 'Квожка работает!')
-    elif message.text == "test":
-        bot.send_message(message.chat.id, 'синхронизация работает')
+    if(check_new_message(message.chat.id, message.text)):
+        if message.text[0:2] == 'оп':
+            if message.text[2] == 'л':
+                bot.send_message(message.chat.id, nikl)
+            elif message.text[2] == 'д':
+                bot.send_message(message.chat.id, nikd)
+            elif message.text[2] == 'ж':
+                bot.send_message(message.chat.id, nikg)
+            elif message.text[2] == 'р':
+                bot.send_message(message.chat.id, nikr)
+        elif (message.text).lower() == 'квожка':
+            bot.send_message(message.chat.id, 'Квожка здесь, чё пристали?')
+        elif message.text == "*+":
+            bot.send_message(message.chat.id, 'Квожка работает!')
+        elif message.text == "test":
+            bot.send_message(message.chat.id, 'синхронизация работает')
 
 bot.polling(none_stop=True, interval=0) # , skip_updates = True - тогда он на прошлые не ответит, ооо, спасибочки вроде бы, ахах :) можно проверить, давай
