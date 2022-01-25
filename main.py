bddbc9cae4cceb43e1c0b8c35978c770a5814b48
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-01-25 20:05:04 +0000

    do good

--- main.py
+++ main.py
@@ -11,25 +11,26 @@ nikd = '@artmv_d'
 nikl = '@lizk1a1'
 nikr = '@r4419'
 
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Заскучали?')
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-        if message.text[0:2] == 'оп':
-            if message.text[2] == 'л':
-                bot.send_message(message.chat.id, nikl)
-            elif message.text[2] == 'д':
-                bot.send_message(message.chat.id, nikd)
-            elif message.text[2] == 'ж':
-                bot.send_message(message.chat.id, nikg)
-            elif message.text[2] == 'р':
-                bot.send_message(message.chat.id, nikr)
-        elif (message.text).lower() == 'квожка':
-            bot.send_message(message.chat.id, 'Квожка здесь, чё пристали?')
-        elif message.text == "*+":
-            bot.send_message(message.chat.id, 'Квожка работает!')
-        elif message.text == "test":
-            bot.send_message(message.chat.id, 'синхронизация работает')
+    if message.text[0:2].lower == 'оп':
+        if message.text[2] == 'л':
+            bot.send_message(message.chat.id, nikl)
+        elif message.text[2] == 'д':
+            bot.send_message(message.chat.id, nikd)
+        elif message.text[2] == 'ж':
+            bot.send_message(message.chat.id, nikg)
+        elif message.text[2] == 'р':
+            bot.send_message(message.chat.id, nikr)
+    elif (message.text).lower() == 'квожка':
+        bot.send_message(message.chat.id, 'Квожка здесь, чё пристали?')
+    elif message.text == "*+":
+        bot.send_message(message.chat.id, 'Квожка работает!')
 
 if __name__ == '__main__':
     bot.skip_pending = True
     bot.infinity_polling()
-    # bot.polling(none_stop=True, interval=0)
