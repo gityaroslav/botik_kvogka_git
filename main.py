56f10a687a91b5ecf381c6c85131e38f84087161
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-02-01 18:03:54 +0000

    доброе утро

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ nikg = '@freak_sqd03'
 nikd = '@artmv_d'
 nikl = '@lizk1a1'
 nikr = '@r4419'
+game_shp=False
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
@@ -19,26 +20,31 @@ def start(m, res=False):
 def handle_text(message):
     new_sms_l=message.text.lower()
     new_sms=message.text
-    if new_sms_l[0:2] == 'оп':
-        if new_sms_l[2] == 'л':
+    if new_sms_l[0:2] == 'оп' and game_shp==False:
+        if new_sms_l[2] == 'л' and game_shp==False:
             bot.send_message(message.chat.id, nikl)
-        elif new_sms_l[2] == 'д':
+        elif new_sms_l[2] == 'д' and game_shp==False:
             bot.send_message(message.chat.id, nikd)
-        elif new_sms_l[2] == 'ж':
+        elif new_sms_l[2] == 'ж' and game_shp==False:
             bot.send_message(message.chat.id, nikg)
-        elif new_sms_l[2] == 'р':
+        elif new_sms_l[2] == 'р' and game_shp==False:
             bot.send_message(message.chat.id, nikr)
-        elif new_sms_l[2:5] == 'все':
+        elif new_sms_l[2:5] == 'все' and game_shp==False:
             bot.send_message(message.chat.id, f'{nikr} {nikd} {nikl} {nikg}')
-    elif new_sms_l == 'квожка':
+    elif new_sms_l == 'квожка' and game_shp==False:
         bot.send_message(message.chat.id, 'Квожка здесь, чё пристали?')
+######################################################
     elif new_sms == "*+":
         bot.send_message(message.chat.id, 'Квожка работает!')
-    elif new_sms_l[0:7]=='отпвчат':
-        bot.send_message(-1001139329557, new_sms[8:])
-        bot.send_message(message.chat.id, 'Отправил!')
     elif new_sms_l[0:11]=='ночь квожка':
         bot.send_message(-1001139329557, f'{nikr} {nikd} {nikl} {nikg}\nВсем спокойной ночи!')
+        bot.send_message(message.chat.id, 'Отправил!')
+    elif new_sms_l[0:11]=='утро квожка':
+        bot.send_message(-1001139329557, f'{nikr} {nikd} {nikl} {nikg}\nВсем доброе утро!')
+        bot.send_message(message.chat.id, 'Отправил!')
+#########################################################
+    elif new_sms_l[0:7]=='отпвчат' and game_shp==False:
+        bot.send_message(-1001139329557, new_sms[8:])
         bot.send_message(message.chat.id, 'Отправил!')
 if __name__ == '__main__':
     bot.skip_pending = True
