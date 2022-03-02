242440daa6d6add4bad77bdedc5f03711aaeeb1e
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-03-02 17:42:23 +0000

    gdhf

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ try:
     cur = conn.cursor()
     bot.send_message(841463984, "Podkl")
 except Exception as e:
-    bot.send_message(841463984, "NEPodkl")
+    bot.send_message(841463984, "Е")
 
 ourchatid=-1001139329557
 id_otchet_chat=-1001750309280
@@ -217,7 +217,9 @@ def handle_text(message):
         bot.send_message(ourchatid, f'Игра Шпион окончена.\nПредателем был(а) {chel_shpion}.\nЛокация называлась {location_shp}')
 #########################################################
     elif new_sms_l=='баланс':
+        bot.send_message(idr, "popal")
         if id_chel==idr:
+            bot.send_message(idr, "popal glubje")
             command = 'select balance from kvg_db where name = "Рося"'
             cur.execute(command)
             balance = cur.fetchone()
