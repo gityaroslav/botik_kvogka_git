a99d625d57a986521fa7bc584303b53a68b85478
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-03-09 15:12:43 +0000

    2

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
+import tgcrypto
 
-app = Client("my_account")
+app = Client(
+    "my_account",
+    api_id=15335811,
+    api_hash="d5432a68519e4ef1c1948b4bd1cf2d8c"
+)
 
 @app.on_message(filters.me)
 def echo(client, message):
