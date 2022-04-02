84a9425fd0f78cda53589622b954d91e19ca7ba5
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-04-02 08:28:01 +0000

    12

--- main.py
+++ main.py
@@ -246,7 +246,8 @@ def handle_text(message):
                     cur.execute(sql_zaprosik)
                     conn.commit()
                 command_sql_zaprosik=sql_zaprosik.find(" ")
-                itog_sql_zaprosika=f"SQL запрос:\nКоманда:{sql_zaprosik[:command_sql_zaprosik+1]}"
+                sql_zaprosik=sql_zaprosik[command_sql_zaprosik+1:]
+                itog_sql_zaprosika=f"SQL запрос:\n―――――\nКоманда: {sql_zaprosik[:command_sql_zaprosik+1]}\n456"
                 bot.send_message(id_chat, itog_sql_zaprosika)
             except Exception as e:
                 bot.send_message(id_chat, f"Что-то пошло не так. Попробуйте заново! {emoji[4]}")
