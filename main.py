b2f6f9dbfd814f98f0c76fcaf97e329365a3a221
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-04-02 16:35:31 +0000

    456

--- main.py
+++ main.py
@@ -258,9 +258,10 @@ def handle_text(message):
                     index_table_sql_zaprosik=sql_zaprosik.find("from")
                     dop1_index_table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+5:].find(" ")
                     if dop1_index_table_sql_zaprosik==-1:
-                        table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+1:]
+                        table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+5:]
                     else:
-                        table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+1:dop1_index_table_sql_zaprosik]
+                        table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+5:dop1_index_table_sql_zaprosik]
+                        zapros_sql_zaprosik=sql_zaprosik[dop1_index_table_sql_zaprosik+1:]
                 elif command_sql_zaprosik=="insert":
                     index_table_sql_zaprosik=sql_zaprosik.find("into")
                     table_sql_zaprosik=sql_zaprosik[index_table_sql_zaprosik+5:]
