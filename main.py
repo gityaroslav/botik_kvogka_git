525f48b30e7dd08e8ae9ebd66f1753e00c2d8019
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-04-10 08:53:19 +0000

    123978

--- main.py
+++ main.py
@@ -579,7 +579,7 @@ try:
                 bot.send_message(int(new_sms_l[index3+1:index4]), str(new_sms_l[index1+1:index2]))
             elif new_sms_l[:21]=="квожка скажи все айди" and id_chel==idr:
                 bot.send_message(id_chat, f"Айди Роси: {idr}\nАйди Даши: {idd}\nАйди Жени: {idg}\nАйди Лизы: {idl}\nАйди Егора: 981661206\nАйди Вари: 1450023923\nАйди Ники: 1039315228\nАйди Полины: 1230762892\n―――――\nАйди основного чата: {ourchatid}\nАйди Валютки: ...\nАйди чата с ошибками: {id_error_chat}")
-            elif new_sms_l[:15]=="квожка рассылка":
+            elif new_sms_l[:15]=="квожка рассылка" and id_chel==idr:
                 text_rassilka=new_sms_l[16:]
                 cur.execute("select id from kvg_db")
                 namebalance = cur.fetchall()
