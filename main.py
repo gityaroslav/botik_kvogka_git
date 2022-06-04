ec174da8c4b90036c29a408971d592c5ad3a1d58
Yar Kek <97759749+gityaroslav@users.noreply.github.com>
2022-06-04 18:25:45 +0000

    123321

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ try:
                 if new_sms_l[2] == 'ж':
                     if len(new_sms_l)==3:
                         bot.delete_message(id_chat, message.message_id)
-                    bot.send_message(id_chat, text=f"@freak\_sqd03 Отметил\(а\): [{message.from_user.first_name}](tg://user?id={id_chel}\)", parse_mode='MarkdownV2')
+                    bot.send_message(id_chat, text=f"@freak\_sqd03 Отметил\(а\): [{message.from_user.first_name}](tg://user?id={id_chel})", parse_mode='MarkdownV2')
                     command123456 = f"update names_keys set key = {sms_count} where name = 'sms_count'"
                     cur.execute(command123456)
                 if new_sms_l[2] == 'р':
